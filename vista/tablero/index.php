bab95da1669c249f49f9121eb18a1b867aba07db
XavierChiguano <67354394+superxavi@users.noreply.github.com>
2022-08-19 10:42:31 -0500

    Delete vista/tablero directory

--- vista/tablero/index.php
+++
@@ -1,8 +0,0 @@
-<?php  include '../template/header.php'?>
-    <!-- Main content -->
-    <section class="content">
-        Pide tu peli Gratis
-    </section>
-    <!-- /.content -->
-    <?php  include '../template/footer.php'?>
-    
