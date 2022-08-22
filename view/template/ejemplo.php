1574cdbae68ba671e04ccf28382e6a1d0e73d900
XavierChiguano <67354394+superxavi@users.noreply.github.com>
2022-08-22 10:18:22 -0500

    Add files via upload

---
+++ view/template/ejemplo.php
@@ -0,0 +1,7 @@
+    <?php  include '../template/header.php'?>
+    <!-- Main content -->
+    <section class="content">
+        HOLA MUDO
+    </section>
+    <!-- /.content -->
+    <?php  include '../template/footer.php'?>
