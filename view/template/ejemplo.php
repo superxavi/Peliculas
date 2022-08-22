0cc6bff049d5ccb7c73a7b469e7209719dcd6cb0
XavierChiguano <67354394+superxavi@users.noreply.github.com>
2022-08-22 10:43:35 -0500

    Delete view/template directory

--- view/template/ejemplo.php
+++
@@ -1,7 +0,0 @@
-    <?php  include '../template/header.php'?>
-    <!-- Main content -->
-    <section class="content">
-        HOLA MUDO
-    </section>
-    <!-- /.content -->
-    <?php  include '../template/footer.php'?>
