b3d0869525163b87b07497f24a31d85cf3bfb210
XavierChiguano <67354394+superxavi@users.noreply.github.com>
2022-08-25 21:07:09 -0500

    Delete vista/tablero directory

--- vista/tablero/index.php
+++
@@ -1,8 +0,0 @@
-<?php  include '../template/header.php'?>
-    <!-- Main content -->
-    <section class="content">
-        HOLAAAA
-    </section>
-    <!-- /.content -->
-    <?php  include '../template/footer.php'?>
-    
